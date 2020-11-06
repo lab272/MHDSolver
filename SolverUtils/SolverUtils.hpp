25e99bb4545faa8dd413f45eaa8e2076749e285d
lab272 <73732483+lab272@users.noreply.github.com>
2020-11-06 10:06:53 +0700

    Add files via upload

---
+++ SolverUtils/SolverUtils.hpp
@@ -0,0 +1,16 @@
+namespace Nektar 
+{
+    namespace SolverUtils
+    {
+        enum EvolutionOperatorType
+        {
+            eNonlinear,
+            eDirect,
+            eAdjoint,
+            eTransientGrowth,
+            eSkewSymmetric,
+            eAdaptiveSFD,
+            eNonlinearDisturb
+        };
+    }
+}
